4ef24c9668ce5b37b8c45d814984eca9cf2b017e
Grant Heffernan <heffergm@gmail.com>
2018-01-06 08:26:42 -0500

    rough draft of proto.rb

---
+++ proto/proto.rb
@@ -0,0 +1,91 @@
+#!/usr/bin/env ruby
+
+require 'bindata'
+
+# the header spec is:
+#  4 byte magic string 'MPS7'
+#  1 byte version
+#  4 byte number of records
+#
+class MpsHeader < BinData::Record
+  endian  :big
+  string  :magic,       :length => 4
+  uint8   :version,     :length => 1
+  uint32  :num_records, :length => 4
+end
+
+# the record spec is:
+#   1 byte record_type
+#   4 byte unix timestamp
+#   8 byte uid
+#
+#   record_type enum:
+#     0x00: Debit
+#     0x01: Credit
+#     0x02: StartAutopay
+#     0x03: EndAutopay
+#
+class MpsRecord < BinData::Record
+  endian  :big
+  uint8   :record_type,   :length => 1
+  uint32  :timestamp,     :length => 4   
+  uint64  :uid,           :length => 8
+  double  :credit,        :length => 8, :onlyif => :is_credit?
+  double  :debit,         :length => 8, :onlyif => :is_debit?
+  string  :start_autopay, :length => 0, :onlyif => :is_start_autopay?
+  string  :end_autopay,   :length => 0, :onlyif => :is_end_autopay?
+
+  def is_debit?
+    return true if record_type == 0x00
+  end
+  def is_credit?
+    return true if record_type == 0x01
+  end
+  def is_start_autopay?
+    return true if record_type == 0x02
+  end
+  def is_end_autopay?
+    return true if record_type == 0x03
+  end
+end
+
+# read the header
+io = File.open('txnlog.dat')
+header = MpsHeader.read(io)
+
+puts 'Magic string: ' + header.magic
+puts 'Version: ' + header.version.to_s
+puts 'Total number of records found: ' + header.num_records.to_s
+
+i = 0
+debits = []
+credits = []
+end_autopays = []
+start_autopays = []
+noted_uid_balance = 0
+
+while i <= header.num_records
+  r = MpsRecord.read(io)
+  puts r
+
+  debits.push(r.debit) unless r.debit == 0.0
+  credits.push(r.credit) unless r.credit == 0.0
+  #end_autopays.push(r.end_autopay) unless r.end_autopay == 0.0
+  #start_autopays.push(r.start_autopay) unless r.start_autopay == 0.0
+
+  # figure out our special friend's balance
+  if r.uid == 2456938384156277127
+    balance = r.credit - r.debit
+    noted_uid_balance = noted_uid_balance + balance
+  end
+  i += 1
+end
+
+total_debits = debits.inject(0){|sum,x| sum + x }
+total_credits = credits.inject(0){|sum,x| sum + x }
+
+puts 'Found UID 2456938384156277127. Balance: ' + noted_uid_balance.to_s
+puts 'Total debits: ' + total_debits.to_s
+puts 'Total credits: ' + total_credits.to_s
+#puts 'Autopays started: ' + start_autopays.length.to_s
+#puts 'Autopays ended: ' + end_autopays.length.to_s
